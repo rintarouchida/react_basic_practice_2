52235cb2409df3b7ea452cd5f474e9b6c287460e
rintarouchida <ww.stamford@gmail.com>
2022-05-16 11:33:42 +0900

    make_todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-import './index.css';
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化
+  const inputText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value="";
+  createIncompleteList(inputText);
+}
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
+
+  // div生成
+  const div = document.createElement('div');
+  div.className = 'list-row';
+
+  // liタグ生成
+  const li = document.createElement('li');
+  li.innerText = text;
+
+  // 完了ボタン生成
+  const completeButton = document.createElement('button');
+  completeButton.innerText = '完了';
+  completeButton.addEventListener('click', () => {
+
+    // div生成
+    // 押されたボタンの親タグ(div)を削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    const addTarget = completeButton.parentNode;
+    //TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    //div以下を初期化
+    addTarget.textContent = null;
+    // liタグ生成
+    const li = document.createElement('li');
+    li.innerText = text;
+    // buttonタグ生成
+    const backButton = document.createElement('button');
+    backButton.innerText = '戻す';
+    backButton.addEventListener('click', () => {
+      deleteFromCompleteList(backButton.parentNode);
+      backTarget = backButton.parentNode;
+      //TODO内容テキストを取得
+      const text = backTarget.firstElementChild.innerText;
+      //div以下を初期化
+      createIncompleteList(text);
+    });
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+    document.getElementById('complete-list').appendChild(addTarget);
+  });
+
+  // 削除ボタン生成
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '削除';
+  // 押されたボタンの親タグ(div)を削除
+  deleteButton.addEventListener('click', () => {
+    deleteFromIncompleteList(deleteButton.parentNode)
+  });
+
+  // divタグの子要素にliを設置
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById('incomplete-list').appendChild(div);
+}
+
+// 未完リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById('incomplete-list').removeChild(target);
+}
+const deleteFromCompleteList = (target) => {
+  document.getElementById('complete-list').removeChild(target);
+}
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
